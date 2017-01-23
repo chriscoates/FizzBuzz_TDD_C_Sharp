71f286c0b26b5d45648ee8ff8056e3faddda17ca
Coates <coates.c@hotmail.com>
2017-01-23 21:37:04 +0000

    adding for loop and method call to main program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzz.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+            Console.ReadLine();
         }
     }
 }
