d88c6629046ea76831a31d1e7533ce7097d932a7
Coates <coates.c@hotmail.com>
2017-01-23 21:11:08 +0000

    first passing test

--- FizzBuzz.Library.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz.Library.Tests
 {
+    [TestFixture]
     public class FizzBuzzerTests
     {
+        [Test]
+        public void Buzzer_When1_Returns1()
+        {
+            //Arrange
+            int input = 1;
+
+            //Act
+            string output = FizzBuzzer.GetValue(input);
+
+            //Assert
+        }
     }
 }
